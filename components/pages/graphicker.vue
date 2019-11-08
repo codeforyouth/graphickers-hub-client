a67a1376dc8c7ed65c9051f75952c9106fc0ffa3
sensu-watson <sensunowatson@gmail.com>
2019-11-09 01:10:20 +0900

    graphickers list

---
+++ components/pages/graphicker.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="media">
+    <svg
+      class="bd-placeholder-img mr-3"
+      width="64"
+      height="64"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid slice"
+      focusable="false"
+      role="img"
+      aria-label="Placeholder: 64x64"
+    >
+      <title>Placeholder</title>
+      <rect width="100%" height="100%" fill="#868e96" />
+      <text x="20%" y="50%" fill="#dee2e6" dy=".3em">64x64</text>
+    </svg>
+    <div class="media-body">
+      <h5 class="mt-0">{{ name }}</h5>
+      {{ introduction }}
+    </div>
+  </div>
+</template>
+
+<style>
+.media {
+  border: solid 1px #000000;
+  border-radius: 10px;
+  padding: 1ex;
+}
+</style>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      default: '名無し'
+    },
+    introduction: {
+      type: String,
+      default: '自己紹介はありません'
+    }
+  }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,62 @@
 <template>
   <div class="container">
-    <header></header>
-    <main></main>
+    <header class="jumbotron">
+      <h1>Graphicker's Hub</h1>
+      <p>グラフィックレコーダー、グラフィックファシリテータと繋がる！！</p>
+    </header>
+    <main>
+      <div class="graphickers-list container">
+        <div class="row">
+          <graphicker
+            class="col-lg-4 col-md-12"
+            :name="name"
+            :introduction="introduction"
+          />
+          <graphicker
+            class="col-lg-4 col-md-12"
+            :name="name"
+            :introduction="introduction"
+          />
+          <graphicker
+            class="col-lg-4 col-md-12"
+            :name="name"
+            :introduction="introduction"
+          />
+        </div>
+      </div>
+      <nav>
+        <ul class="pagination justify-content-center">
+          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
+    </main>
     <footer></footer>
   </div>
 </template>
+
+<style>
+.graphickers-list {
+  padding: 1ex;
+}
+</style>
+
+<script>
+import Vue from 'vue'
+import graphicker from '~/components/pages/graphicker.vue'
+
+export default Vue.extend({
+  components: {
+    graphicker
+  },
+  data() {
+    return {
+      name: '渡辺 賢',
+      introduction: 'Code for Nagoya, Aichi, Youthで活躍中。'
+    }
+  }
+})
+</script>
