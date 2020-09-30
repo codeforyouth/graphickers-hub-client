c2e06e0354cd80e1cc8897d3c078a285af20d5a1
sensu-watson <sensunowatson@gmail.com>
2020-09-30 22:34:31 +0900

    画像拡大機能追加

---
+++ components/modal/showAvatarModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-modal id="bv-modal-show-avatar" size="xl" centered hide-footer>
+    <b-img class="b-img" :src="imgUrl"></b-img>
+  </b-modal>
+</template>
+
+<style>
+.b-img {
+  object-fit: contain;
+  width: 100%;
+  height: 80vh;
+
+  margin-left: 0;
+  margin-right: 0;
+}
+</style>
+
+<script>
+import Vue from 'vue'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  computed: {
+    ...mapState('portfolios', {
+      imgUrl: 'avatarUrl'
+    })
+  }
+})
+</script>
